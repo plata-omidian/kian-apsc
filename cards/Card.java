83334dfdde0f5b9ed4b5f8ffcc51c80108b7f75c
Kian Omidian <kospaids@gmail.com>
2025-11-14 23:16:40 +0000

    i made the card class

--- cards/Card.java
+++ cards/Card.java
@@ -1,6 +1,23 @@
 package cards;
 
+import piglatin.string;
+
 public class Card {
-
-
+//fields   
+    private int value;
+    private int suit;
+    private static String[] suits={"♦","♣","♥","♠"};
+    private static String[] values={"a", "2", "3", "4", "5", "6", "7", "8", "9", "10", "j", "q", "k"};
+//constructor
+    public Card(int suit, int value){
+        this.value=value;
+        this.suit=suit;
+    }
+//methods
+    public int getValue(){
+        return(value);
+    }
+    public String tooString(){
+        return(suits[suit]+values[value]);
+    }
 }
