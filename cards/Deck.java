740645a1e1f611af425fd778ba83a5389282ebd2
Kian Omidian <kospaids@gmail.com>
2025-11-17 23:08:08 +0000

    start deck class

--- cards/Deck.java
+++ cards/Deck.java
@@ -1,6 +1,15 @@
 package cards;
 
 public class Deck {
-
-
+//fields
+    //private static String[] deck=
+//constructor
+    public Deck(){
+        for(int i=0;i<=52;i++){
+            Card cd = new Card();
+        }
+    }
+//methods
+    public String print(){
+    }
 }
