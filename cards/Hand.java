a841cbc6cabd20c991655c82ad0d337c942ca206
Kian Omidian <kospaids@gmail.com>
2025-11-18 17:12:15 +0000

    halfway through hand

--- cards/Hand.java
+++ cards/Hand.java
@@ -1,8 +1,27 @@
 package cards;
 
+import javax.smartcardio.Card;
+
 public class Hand {
-    int max;
+    private int max;
+    private static String[] hand;
+//constructer
     public Hand(int max){
         this.max=max;
+    }
+//methods
+    public void add(Card){
+        if(hand.length<max){
+            hand.add(Card);
+        }
+        else{
+            System.out.println("no can do bud, your hand is full");
+        }
+    }
+    public int length(){
+        return(hand.length);
+    }
+    public String get(){
+        return(hand[]);
     }
 }
