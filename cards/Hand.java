33f7386bcea7a725496e40eefc2012e0ef1e8c39
Kian Omidian <kospaids@gmail.com>
2025-11-19 04:31:09 +0000

    hand finished

--- cards/Hand.java
+++ cards/Hand.java
@@ -26,15 +26,37 @@ public class Hand {
     public int length(){
         return topNum;
     }
-    public Card get(int index){
+     public Card get(int index) {
+        if (index < 0 || index >= topNum) {
+            return null;
+        }
         return hand[index];
     }
-    public String toString(){
-        String result="";
-        for (int i=0;i<topNum;i++)
-        {
-            result+=hand[i].toString()+" ";
+      public String toString() {
+        String result = "";
+        for (int i = 0; i < topNum; i++) {
+            if (i > 0) {
+                result += " ";
+            }
+            result += hand[i].toString();
         }
         return result;
+    }
+      public Card remove(int index) {
+        if (index < 0 || index >= topNum) {
+            return null;
+        }
+
+        Card removed = hand[index];
+
+        // shift everything after index one spot to the left
+        for (int i = index; i < topNum - 1; i++) {
+            hand[i] = hand[i + 1];
+        }
+
+        hand[topNum - 1] = null;   // optional, just clears the last slot
+        topNum--;
+
+        return removed;
     }
 }
