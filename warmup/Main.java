5baa8ac7bbc191dde9cfc1117f6a007832feb21b
Kian Omidian <kospaids@gmail.com>
2026-01-08 22:50:51 +0000

    i did the first one

--- warmup/Main.java
+++ warmup/Main.java
@@ -4,7 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         // Your code here.
-
+        int j=0;
+        int sum=0;
+        while (j<1000){
+            if(j%3==0||j%5==0){
+                sum+=j;
+            }
+            j++;
+        }
+        System.out.println(sum);
     }
 }
 //hi
