8a83b10262eda235c23a5be791ea4aa0703dd720
Kian Omidian <kospaids@gmail.com>
2026-01-26 23:13:08 +0000

    i started insertion

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -3,6 +3,8 @@ package sorting;
 public class InsertionSort implements Sorter {
 
     public void sort(int[] input) {
+        int[] temp;
         System.out.println("Insertion Sort!!!");
+        for()
     }
 }
