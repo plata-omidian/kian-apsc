ead982ca087e6fd5e2d4494138a061f4fea49e15
Kian Omidian <kospaids@gmail.com>
2026-01-27 23:15:32 +0000

    testing insertion

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -3,14 +3,15 @@ package sorting;
 public class InsertionSort implements Sorter {
 
     public void sort(int[] input) {
-        int[] temp;
-        int j=1;
         System.out.println("Insertion Sort!!!");
-        for(int i=0;i<input.length;i++){
-            if(input[i]>input[i+j]){
-                j++;
-
+        for(int i = 1; i < input.length; i++) {
+            int temp = input[i];
+            int j = i - 1;
+            while(j >= 0 && input[j] > temp) {
+                input[j + 1] = input[j];
+                j--;
             }
+            input[j + 1] = temp;
         }
     }
 }
