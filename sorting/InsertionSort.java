3214fadbd2a996a0da012797e33af11161ede518
Kian Omidian <kospaids@gmail.com>
2026-01-27 23:05:34 +0000

    im still doing insertion

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -4,7 +4,13 @@ public class InsertionSort implements Sorter {
 
     public void sort(int[] input) {
         int[] temp;
+        int j=1;
         System.out.println("Insertion Sort!!!");
-        for()
+        for(int i=0;i<input.length;i++){
+            if(input[i]>input[i+j]){
+                j++;
+
+            }
+        }
     }
 }
