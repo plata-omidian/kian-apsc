0ba013501260de27039c6c7fa480844fe5568280
Kian Omidian <kospaids@gmail.com>
2026-01-29 21:51:10 +0000

    i cant do insertion im moving on

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -4,14 +4,15 @@ public class InsertionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Insertion Sort!!!");
-        for(int i = 1; i < input.length; i++) {
+        for (int i = 1; i < input.length; i++) {
             int temp = input[i];
             int j = i - 1;
-            while(j >= 0 && input[j] > temp) {
+
+            while (j >= 0 && input[j] > temp) {
                 input[j + 1] = input[j];
                 j--;
-            }
-            input[j + 1] = temp;
         }
+        input[j + 1] = temp;
+    }
     }
 }
