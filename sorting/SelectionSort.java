f3eaed3e3492b0ec5603848d9b546e2b971e594b
Kian Omidian <kospaids@gmail.com>
2026-01-29 22:10:47 +0000

    im trying selection

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -4,5 +4,16 @@ public class SelectionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Selection Sort!!!");
+       for(int i=0;i<input.length;i++){
+        int small=i;
+        for(int j=i+1;j<input.length;j++){
+            if(input[j]<input[small]){
+                small=j;
+            }
+        }
+        int temp=input[i];
+        input[i]=input[small];
+        input[small]=temp;
+       }
     }
 }
