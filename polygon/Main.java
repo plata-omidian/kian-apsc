2eb1bd7c4bf010dfd7bf288573f985bc44c87a25
Kian Omidian <kospaids@gmail.com>
2026-03-06 23:17:54 +0000

    im cooking

--- polygon/Main.java
+++ polygon/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String [] args)
     {
         IrregularPolygon myPolygon = new IrregularPolygon();
-        double[][] parallelogramPoints = { { 20, 10 }, { 70, 20 }, { 50, 50 }, { 0, 40 } };
+        double[][] parallelogramPoints = { { 90, -20 }, { 100, 60 }, { 90, 30 }, { 60, 10 } };
         for (double[] p: parallelogramPoints){
             Point2D.Double point = new Point2D.Double(p[0],p[1]);
             myPolygon.add(point);
