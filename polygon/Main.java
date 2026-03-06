e1e0f600aea6baa5a69d1453fcfc8ad41fb39547
Kian Omidian <kospaids@gmail.com>
2026-03-06 23:15:24 +0000

    I DID IT

--- polygon/Main.java
+++ polygon/Main.java
@@ -1,9 +1,15 @@
 package polygon;
 
+import java.awt.geom.*; // for Point2D.Double
 public class Main {
     public static void main(String [] args)
     {
         IrregularPolygon myPolygon = new IrregularPolygon();
+        double[][] parallelogramPoints = { { 20, 10 }, { 70, 20 }, { 50, 50 }, { 0, 40 } };
+        for (double[] p: parallelogramPoints){
+            Point2D.Double point = new Point2D.Double(p[0],p[1]);
+            myPolygon.add(point);
+        }
         myPolygon.draw();
 
         TestSuite.run();
